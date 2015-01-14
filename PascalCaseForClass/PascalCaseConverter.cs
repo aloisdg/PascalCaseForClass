1bf1ee536a130993c845ea503d63abbd9a58749f
aloisdg <aloisdegouvello@live.fr>
2015-01-14 16:32:46 +0100

    Convert basic line to PascalCase

---
+++ PascalCaseForClass/PascalCaseConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PascalCaseForClass
+{
+    // http://wpftutorial.net/ValueConverters.html
+    [ValueConversion(typeof(string), typeof(string))]
+    public class PascalCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var source = value as string;
+            return !String.IsNullOrEmpty(source)
+                ? ConvertToPascalCase(source, culture)
+                : value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        // http://stackoverflow.com/questions/1206019/converting-string-to-title-case-in-c-sharp
+        private static string ConvertToPascalCase(string source, CultureInfo culture)
+        {
+            return culture.TextInfo.ToTitleCase(source);
+        }
+    }
+}
